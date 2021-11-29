c3bfd3c70e1e19c2c4fa7174c3e9e80e589e3f05
devnote-dev <pokeraven4@gmail.com>
2021-11-29 21:52:21 +0000

    fix(cmd): commands -> cmd

---
+++ cmd/std.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var VersionCmd = &cobra.Command{
+	Use:     "version",
+	Short:   "Shows the current version of Soar",
+	Long:    "Shows the current version of Soar as well as contributors",
+	Aliases: []string{"v"},
+	Run: func(cmd *cobra.Command, _ []string) {
+		fmt.Printf("Soar version %s (%s)\n\n© 2021 devnote-dev", "0.0.1a", "27/11/2021 02:39")
+	},
+}
