65f2dac3b31d9d83a01b892838f5574f24defec8
devnote-dev <pokeraven4@gmail.com>
2021-12-02 15:23:28 +0000

    feat(app): start on application subcmds

---
+++ app/application.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/pteropackages/soar/soar"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+var GetUsersCmd = &cobra.Command{
+	Use:   "get-users",
+	Short: "Gets a list of users from the panel",
+	Long:  "Gets a list of users from the panel, following the Pterodactyl pagination system",
+	Run: func(cmd *cobra.Command, args []string) {
+		session := soar.NewSession("application")
+		// if len(args) > 0 {
+		// 	// TODO
+		// }
+		data, err := session.Request("/api/application/users", "GET", nil)
+		if err != nil {
+			panic(err)
+		}
+
+		var parsed map[string]string
+		yaml.Unmarshal(data, &parsed)
+		fmt.Printf("%v\n", parsed)
+	},
+}
+
+func init() {
+	flags := GetUsersCmd.Flags()
+	flags.String("id", "", "the internal ID of the user")
+	flags.String("uuid", "", "the UUID of the user")
+	flags.String("username", "", "the username to query by")
+	flags.String("email", "", "the email to query by")
+	flags.String("external", "", "the external ID of the user")
+	flags.Bool("servers", true, "whether to include servers")
+	flags.Bool("s", true, "")
+}
