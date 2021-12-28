51fba7595f996cd8b9f22c6ebab419e526e55ed2
devnote-dev <pokeraven4@gmail.com>
2021-12-28 22:22:59 +0000

    feat(config): implement setup command

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"runtime"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -11,7 +12,7 @@ var GroupCommand = &cobra.Command{
 	Use: "config",
 }
 
-var InfoCmd = &cobra.Command{
+var infoCmd = &cobra.Command{
 	Use:   "info",
 	Short: "Gets information about the Soar configuration.",
 	Long:  "Gets information about the Soar configuration.",
@@ -37,6 +38,70 @@ var InfoCmd = &cobra.Command{
 	},
 }
 
+var setupCommand = &cobra.Command{
+	Use:   "setup",
+	Short: "Sets up the Soar configuration directories and files.",
+	Long:  "Sets up the Soar configuration directories and files.",
+	Run: func(cmd *cobra.Command, _ []string) {
+		_, err := GetConfig()
+		if err == nil {
+			fmt.Print("Config file already found, do you want to reset? (y/n) ")
+			if !loopInputA() {
+				fmt.Println("Cancelling setup...")
+				return
+			}
+		}
+
+		fmt.Print("\nWhere should the files be setup?\nLeave blank to use the default path: ")
+		fp := loopInputB()
+		if fp == "" {
+			fmt.Println("Using default Soar path instead")
+			if runtime.GOOS == "windows" {
+				fp = "C:\\soar"
+			} else {
+				fp = "/soar"
+			}
+		}
+
+		err = CreateEnv(fp)
+		if err != nil {
+			fmt.Printf("soar: %s", err.Error())
+			return
+		}
+
+		fmt.Printf("Successfully setup soar directories at: %s", fp)
+	},
+}
+
+func loopInputA() bool {
+	var res string
+	for {
+		fmt.Scanln(&res)
+		switch strings.ToLower(res) {
+		case "y", "yes":
+			return true
+		case "n", "no":
+			return false
+		default:
+			continue
+		}
+	}
+}
+
+func loopInputB() string {
+	var res string
+	for {
+		fmt.Scanln(&res)
+		if res == "" {
+			return ""
+		}
+		if Exists(res) {
+			return res
+		}
+	}
+}
+
 func init() {
-	GroupCommand.AddCommand(InfoCmd)
+	GroupCommand.AddCommand(infoCmd)
+	GroupCommand.AddCommand(setupCommand)
 }
